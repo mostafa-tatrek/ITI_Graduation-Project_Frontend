a992eaaeb6603c2676598d43991bd533cb208769
MariamHosini <hossinimariam93@gmail.com>
2025-06-03 13:05:11 +0300

    Your clear and meaningful commit message

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -43,6 +43,6 @@ export class HeaderComponent implements OnInit {
     this.auth.loggedUserId = null;
   }
   GoToProfile() {
-    this._router.navigateByUrl(`UserProfile/${this.auth.loggedUserId}`);
+    this._router.navigateByUrl(`UserProfile/${this.userid}`);
   }
 }
